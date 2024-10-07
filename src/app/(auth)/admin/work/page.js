f81fb57ac088e3b3c14f37d5572634bd8b59479b
ikbarpuanjito <ikbarpuanjito@gmail.com>
2024-10-07 21:22:15 +0700

    Backend

--- src/app/(auth)/admin/work/page.js
+++ src/app/(auth)/admin/work/page.js
@@ -1,6 +1,18 @@
+"use client"
+import { useState } from 'react';
 import Card from '../../../../components/card';
 
 export default function AdminWork() {
+  const [data, setData] = useState({
+    title:'',
+    employeType:'',
+    companyName:'',
+    location:'',
+    startDate:'',
+    endDate:'',
+  });
+  
+
   const optEmployeType = [
     {label:'Full Time', value:'full-time'},
     {label:'Part Time', value:'part-time'},
@@ -8,56 +20,112 @@ export default function AdminWork() {
     {label:'Internship', value:'internship'}
   ]
 
-    return (<>
-        <Card title="Work Form" className="pb-5">
-          <div className="w-full my-2">
-              <label>Title</label>
-              <input type="text" className="w-full border my-input-text"/>
-          </div>
+  const optLocation = [
+    {label:'Onsite', value:'onsite'},
+    {label:'WFH', value:'wfh'},
+  ]
 
-          <div className="w-full my-2">
-              <label>Employe Type</label>
-              <select  className="w-full border my-input-text">
-                {
-                  optEmployeType && 
-                    optEmployeType.map((item)=>
-                      <option value={item.value}>{item.label}</option>
-                    )
-                }
-              </select>
-          </div>
+  const inputHandler= (e) =>{
+    setData({...data, [e.target.name]: e.target.value })
+  }
 
-          <div className="w-full my-2">
-              <label>Company Name</label>
-              <input type="text" className="w-full border my-input-text"/>
-          </div>
+  async function onSubmitData() {
+    try{
+      let res = await fetch('/api/work', {
+        method:'POST',
+        body: JSON.stringify(data),
+      })
+      let resData = await res.json()
+      if(!resData.data){
+        throw Error(resData.message)
+      }
+      alert("Data berhasil disimpan dengan id \n"+ resData.data.insertedId)
+    }catch(err){
+      console.error("ERR", err.message)
+      alert(err.message)
+    }
+}
 
-          <div className="w-full my-2">
-              <label>Location</label>
-              <select  className="w-full border my-input-text">
-                <option>Onsite</option>
-              </select>
-          </div>
+  return (<>
+      <Card title="Work Form" className="pb-5">
+        <div className="w-full my-2">
+            <label>Title</label> 
+            <input 
+              type="text" 
+              name='title'
+              value={data.title}
+              onChange={inputHandler}
+              className="w-full border my-input-text"/>
+        </div>
 
-          <div className="w-full my-2">
-              <label>Start Date</label>
-              <input type="date" className="w-full border my-input-text"/>
-          </div>
+        <div className="w-full my-2">
+            <label>Employe Type</label>
+            <select  
+              name='employeType' 
+              onChange={inputHandler}
+              className="w-full border my-input-text">
+              {
+                optEmployeType && 
+                  optEmployeType.map((item, key)=>
+                    <option key={key} value={item.value}>{item.label}</option>
+                  )
+              }
+            </select>
+        </div>
 
-          <div className="w-full my-2">
-              <label>End Date</label>
-              <input type="date" className="w-full border my-input-text"/>
-          </div>
 
-          <button className="mx-1 h-9 items-center justify-center px-4  rounded-md bg-amber-500">
-              <label>Submit Data</label>
-          </button>
+        <div className="w-full my-2">
+            <label>Company Name</label>
+            <input 
+              name='companyName' 
+              type="text" 
+              onChange={inputHandler}
+              className="w-full border my-input-text"/>
+        </div>
+
+        <div className="w-full my-2">
+            <label>Location</label>
+            <select 
+              name='location'
+              onChange={inputHandler}
+              className="w-full border my-input-text">
+              {
+                optLocation && 
+                optLocation.map((item, key)=>
+                    <option key={key} value={item.value}>{item.label}</option>
+                  )
+              }
+            </select>
+        </div>
+
+        <div className="w-full my-2">
+            <label>Start Date</label>
+            <input 
+              name='startDate'
+              onChange={inputHandler}
+              type="date" 
+              className="w-full border my-input-text"/>
+        </div>
+
+        <div className="w-full my-2">
+            <label>End Date</label>
+            <input 
+              name='endDate'
+              onChange={inputHandler}
+              type="date" 
+              className="w-full border my-input-text"/>
+        </div>
+
+        <button 
+          onClick={onSubmitData}
+          className="mx-1 h-9 items-center justify-center px-4  rounded-md bg-amber-500">
+            <label>Submit Data</label>
+        </button>
         </Card>
         
         <Card title="List of Work" style="mt-5">
-          asdf
+          
         </Card>
       </>
     );
-}
-  
+  }
